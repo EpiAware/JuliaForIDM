6b7f0c3ca665e863a3894c89b7d9d53897503f8f
Sam <s.e.abbott12@gmail.com>
2024-12-16 13:46:57 +0000

    review skeleton

--- src/JuliaForIDM.jl
+++ src/JuliaForIDM.jl
@@ -1,27 +1,5 @@
 module JuliaForIDM
 
-using Distributions
-using DifferentialEquations
-using Parameters
-
-export SimulationParameters, run_simulation
-
-@doc raw"""
-    SimulationParameters
-
-Structure holding the parameters for disease simulation models.
-
-# Fields
-- `β::Float64`: transmission rate
-- `γ::Float64`: recovery rate
-- `N::Int64`: population size
-"""
-@with_kw struct SimulationParameters
-    β::Float64
-    γ::Float64
-    N::Int64
-end
-
-# Additional functions will go here...
+include("docstrings.jl")
 
 end
